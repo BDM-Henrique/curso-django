affe0893472453011e72117ea2a9077c8a2d91db
Henrique <henrique1jose@gmail.com>
2024-03-20 16:44:01 -0300

    Exemplo de tmeplate temp

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name' : 'José Henrique',
+    })
 
 def sobre(request):
     return HttpResponse('SOBRE')
 
 def contato(request):
-    return HttpResponse('CONTATO')
+    return render(request, 'me-apague/temp.html' )
